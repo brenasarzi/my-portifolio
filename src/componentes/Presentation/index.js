924d71423e62478f2816cec0046cd1f682e3e856
Brena Sarzi <sarzibrena@gmail.com>
2022-12-05 11:05:50 -0300

    create cards

--- src/componentes/Presentation/index.js
+++ src/componentes/Presentation/index.js
@@ -1,9 +1,31 @@
 import './Presentation.css'
 
-function Presentation(){
+function Presentation() {
     return (
         <div className="apresentacao">
             <h1>Hello! My name is Brena and I'm a web developer</h1>
+            <h2>Here are some of my skills.</h2>
+            <div className='cards-skills'>
+                <div className='card'>
+                    <img src="/imagens/html.png" />
+                </div>
+                <div className='card'>
+                    <img src="/imagens/css.png" />
+
+                </div>
+                <div className='card'>
+                    <img src="/imagens/js.png" />
+
+                </div>
+                <div className='card'>
+                    <img src="/imagens/react.png" />
+
+                </div>
+                <div className='card'>
+                    <img src="/imagens/typescript.png" />
+
+                </div>
+            </div>
         </div>
     )
 }
